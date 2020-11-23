a05a19906694b8cc53da2e2a96c0de3bde619d86
Richardson Bruno <richardsonbruno@pop-os.localdomain>
2020-11-23 14:54:26 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8f10f0c925d81755ac349917477e4338'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '64d7448c-61f7-4bf5-b14b-1c64d845b51a'
+};
